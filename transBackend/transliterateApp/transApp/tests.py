a530902a40d8d6e4e0b32a7b9c320c9608e19323
Harsh Singh Sikarwar <89659272+Harshsikarwar@users.noreply.github.com>
2025-10-08 14:20:17 +0530

    Update tests.py
    test update

--- transBackend/transliterateApp/transApp/tests.py
+++ transBackend/transliterateApp/transApp/tests.py
@@ -1,10 +1,13 @@
-#from django.test import TestCase
-
-# Create your tests here.
 import requests
 
-url = "http://127.0.0.1:8000/api/extract-text/"
-files = {"image": open("C:\\Users\\Disha\\Downloads\\Traffic-Sign-Board.jpg", "rb")}
-response = requests.post(url, files=files)
+url = "http://127.0.0.1:8000/api/extract-text/Devanagari/"
 
-print(response.json())
+with open("C:\\Users\\Disha\\Downloads\\board.jpg", "rb") as img_file:
+    files = {"image": img_file}
+    response = requests.post(url, files=files)
+
+print("Status Code:", response.status_code)
+
+
+json_data = response.json()
+print(json_data)
